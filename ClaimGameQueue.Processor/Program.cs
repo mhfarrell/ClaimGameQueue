623bea5fa3de476e2d546f590db551be5a8125ab
mhfarrell <matthew.farrell@students.plymouth.ac.uk>
2018-01-11 15:49:52 +0000

    remover/processor

---
+++ ClaimGameQueue.Processor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClaimGameQueue.Processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ClaimGameQueue.Remover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClaimGameQueue.Remover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
