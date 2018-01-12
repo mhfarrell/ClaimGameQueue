b1b5faadd3f3cb73e3437223fbd5c9ba2d03a3d0
mhfarrell <matthew.farrell@students.plymouth.ac.uk>
2018-01-12 20:51:32 +0000

    tester

---
+++ ClaimGameQueue.Tester/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+
+namespace ClaimGameQueue.Tester
+{
+    class Receive
+    {
+        public static void Main()
+        {
+            int i = 0;
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "claims",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: true,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0} queue no: {1}", message, i);
+                    i++;
+                };
+                channel.BasicConsume(queue: "claims",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+} }
+}
